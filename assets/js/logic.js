640254e42f83d6d95a776ae8fcb0cc0b41c1f23f
CarpeDiemNan! <nanurci@gmail.com>
2017-12-13 20:19:00 -0500

    saving

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,18 +1,41 @@
 //      VARIABLES
 topics = ["Dog", "Pony", "Puppy", "Cat", "Fish", "Bird"];
-
-var queryURLBase = "https://api.giphy.com/v1/gifs/search?api_key=0DCbLqFsuTrcFYsBerOEbG7vYEzptnsg&q=funny+animals&limit=10&offset=0&rating=G&lang=en";
+var searchItem = "pony";
+// var queryURLBase = "https://api.giphy.com/v1/gifs/search?api_key=0DCbLqFsuTrcFYsBerOEbG7vYEzptnsg&q=funny+animals&limit=10&offset=0&rating=G&lang=en";
+var queryURLBase = "https://api.giphy.com/v1/gifs/search?api_key=0DCbLqFsuTrcFYsBerOEbG7vYEzptnsg&limit=10&offset=0&rating=G&lang=en&q="+ searchItem;
 //      FUNCTIONS
+// READ thru array, send a button to page for each element
+function popButtons(){
+    for(var i = 0; i < topics.length; i++){
+        
+        var wellSection = $('<button>');
+			wellSection.addClass("btn btn-default");
+			wellSection.attr('id', topics[i]);
+            wellSection.text(topics[i]);            
+            console.log(topics[i]);
+            
+			$("#buttonArea").append(wellSection);      
+            
+			// attach content to appropriate well
+			// $("#articleWell-" + i).append(NYTData.response.docs[i].headline.main)
+			// $("#articleWell-" + i).append(NYTData.response.docs[i].pub_date);
+			// $("#articleWell-" + i).append(NYTData.response.docs[i].section_name);
+			// $("#articleWell-" + i).append(NYTData.response.docs[i].web_url);        
+    }
+}
+
+
+
+
 function runQuery(queryURL){
 	$.ajax({url: queryURL, method: "GET"})
 		.done(function(giphyData) {
 			// clear the wells from the previous run
 			// $("#wellSection").empty();
 			for(var i = 0; i < topics.length; i++){
-			console.log(giphyData);
-			// console.log(NYTData);
-
+			console.log(giphyData);	 
 			// start dynamically dumping to html
+            $("#wellSection" + i).html(giphyData.data[i].bitly_gif_url);
 			// var wellSection = $('<div>');
 			// wellSection.addClass("well");
 			// wellSection.attr('id', 'articleWell-' + i);
@@ -26,6 +49,19 @@ function runQuery(queryURL){
 		})
 }
 //      MAIN PROCESSES
-runQuery(queryURLBase);
+// runQuery(queryURLBase);
+// POPULATE initial buttons
+popButtons();
+// LISTEN for button clicks
+$(document).on('click', ".btn", function(){
+    console.log("This is a " + this.id);
+    searchItem = this.id;    
+    queryURLBase = "https://api.giphy.com/v1/gifs/search?api_key=0DCbLqFsuTrcFYsBerOEbG7vYEzptnsg&limit=10&offset=0&rating=G&lang=en&q="+ searchItem;
+     console.log(queryURLBase);
+    runQuery(queryURLBase);
+    return false;
+})
+
+
 
  
