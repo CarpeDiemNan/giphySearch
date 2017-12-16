044f21e8173ad7e1828c4350a5b1d1e242a25a59
CarpeDiemNan! <nanurci@gmail.com>
2017-12-15 22:40:38 -0500

    saving search

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,9 +1,16 @@
+// ******************************************************************
+// ******************************************************************
 //      VARIABLES
 topics = ["Dog", "Pony", "Puppy", "Cat", "Fish", "Bird"];
 var searchItem = "pony";
+
 // var queryURLBase = "https://api.giphy.com/v1/gifs/search?api_key=0DCbLqFsuTrcFYsBerOEbG7vYEzptnsg&q=funny+animals&limit=10&offset=0&rating=G&lang=en";
 var queryURLBase = "https://api.giphy.com/v1/gifs/search?api_key=0DCbLqFsuTrcFYsBerOEbG7vYEzptnsg&limit=10&offset=0&rating=G&lang=en&q="+ searchItem;
+
+// ********************************************************************
+// ********************************************************************
 //      FUNCTIONS
+
 // READ thru array, send a button to page for each element
 function popButtons(){
     for(var i = 0; i < topics.length; i++){
@@ -14,38 +21,44 @@ function popButtons(){
             wellSection.text(topics[i]);            
             console.log(topics[i]);
             
-			$("#buttonArea").append(wellSection);      
-            
-			// attach content to appropriate well
-			// $("#articleWell-" + i).append(NYTData.response.docs[i].headline.main)
-			// $("#articleWell-" + i).append(NYTData.response.docs[i].pub_date);
-			// $("#articleWell-" + i).append(NYTData.response.docs[i].section_name);
-			// $("#articleWell-" + i).append(NYTData.response.docs[i].web_url);        
+			$("#buttonArea").append(wellSection); 
     }
 }
-
-
-
 
 function runQuery(queryURL){
 	$.ajax({url: queryURL, method: "GET"})
 		.done(function(giphyData) {
 			// clear the wells from the previous run
 			// $("#wellSection").empty();
-			for(var i = 0; i < topics.length; i++){
-			console.log(giphyData);	 
-			// start dynamically dumping to html
-            $("#wellSection" + i).html(giphyData.data[i].bitly_gif_url);
-			// var wellSection = $('<div>');
-			// wellSection.addClass("well");
-			// wellSection.attr('id', 'articleWell-' + i);
-			// $("#wellSection").append(wellSection);
-			// attach content to appropriate well
-			// $("#articleWell-" + i).append(NYTData.response.docs[i].headline.main)
-			// $("#articleWell-" + i).append(NYTData.response.docs[i].pub_date);
-			// $("#articleWell-" + i).append(NYTData.response.docs[i].section_name);
-			// $("#articleWell-" + i).append(NYTData.response.docs[i].web_url);
-		}
+			$("#imageArea").empty();
+			
+			for(var i = 0; i < 10; i++){
+			console.log(giphyData);	
+
+			 // Creating a div to hold the animal pictures
+          	var pictureDiv = $("<div class='pictures'>");
+
+			// store the rating data
+			var rating = giphyData.data[i].rating;
+
+			// create element to have rating displayed
+			var pOne = $("<p>").text("Rating: " + rating);
+
+			// displaying the rating
+			pictureDiv.append(pOne);
+
+			// Retrieving the URL for the image
+          	var imgURL = giphyData.data[i].images.original_still.url;
+
+          	// Creating an element to hold the image
+          	var image = $("<img>").attr("src", imgURL);
+
+          	// Appending the image
+          	pictureDiv.append(image);
+
+          	 // Putting the entire movie above the previous movies
+          	$("#imageArea").prepend(pictureDiv);             
+			}
 		})
 }
 //      MAIN PROCESSES
@@ -59,9 +72,13 @@ $(document).on('click', ".btn", function(){
     queryURLBase = "https://api.giphy.com/v1/gifs/search?api_key=0DCbLqFsuTrcFYsBerOEbG7vYEzptnsg&limit=10&offset=0&rating=G&lang=en&q="+ searchItem;
      console.log(queryURLBase);
     runQuery(queryURLBase);
-    return false;
+    // return false;
 })
 
 
 
- 
+ // attach content to appropriate well
+			// $("#articleWell-" + i).append(NYTData.response.docs[i].headline.main)
+			// $("#articleWell-" + i).append(NYTData.response.docs[i].pub_date);
+			// $("#articleWell-" + i).append(NYTData.response.docs[i].section_name);
+			// $("#articleWell-" + i).append(NYTData.response.docs[i].web_url);        
